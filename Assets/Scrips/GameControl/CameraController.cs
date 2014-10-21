c68a117e8f1b9f58e94820107151ad69cfde96ce
Sandro Amaral <sandro123iv@gmail.com>
2014-10-21 18:18:38 +0100

    Added headbob when moving

--- Assets/Scrips/GameControl/CameraController.cs
+++ Assets/Scrips/GameControl/CameraController.cs
@@ -8,17 +8,44 @@ public class CameraController : MonoBehaviour {
 	private float rotationY;
 
 	private GameController gameController;
+	private CharacterController controller;
+
+	public float headbobSpeed;
+	public float headbobStepCounter;
+	public Vector3 parentLastPos;
+	public float headbobAmountX;
+	public float headbobAmountY;
+	public float eyeHeightRacio;
+	public float currentAimRacio = 1; // Used when aiming weapons to define "aim level"
 
 	void Start() {
 		gameController = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<GameController>();
+		controller = transform.parent.GetComponent<CharacterController>();
 	}
 
 	void Awake() {
 		// Reset those variables, to prevent the camera from spinning when the cursor is locked
 		Input.ResetInputAxes();
+
+		parentLastPos = transform.parent.position;
 	}
 
-	void FixedUpdate() {
+	void Update() {
+		if(controller.isGrounded)
+			headbobStepCounter += Vector3.Distance(parentLastPos, transform.parent.position) * headbobSpeed;
+
+		//transform.localPosition.x = Mathf.Sin(headbobStepCounter) * headbobAmountX * currentAimRacio;
+		//transform.localPosition.y = (Mathf.Cos(headbobStepCounter * 2) * headbobAmountY * -1 * currentAimRacio) + (transform.parent.localScale.y * eyeHeightRacio) - (transform.parent.localScale.y / 2);
+		transform.localPosition = new Vector3(
+				Mathf.Sin(headbobStepCounter) * headbobAmountX * currentAimRacio,
+				(Mathf.Cos(headbobStepCounter * 2) * headbobAmountY * -1 * currentAimRacio) + (transform.parent.localScale.y * eyeHeightRacio) - (transform.parent.localScale.y / 2),
+				transform.localPosition.z
+			);
+
+
+		parentLastPos = transform.parent.position;
+
+
 		if(!gameController.stopMovement()) {
 			rotationY += Input.GetAxis("Mouse Y") * lookSpeed * Time.fixedDeltaTime;
 			rotationY = Mathf.Lerp(rotationY, Mathf.Clamp(rotationY, -90f, 90f), lookSpeed * Time.fixedDeltaTime);
