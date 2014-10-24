c83766a0e9ba912ee48aab03cca84a8da8b7ed92
Sandro Amaral <sandro123iv@gmail.com>
2014-10-24 22:48:34 +0100

    Updated terrain!

--- Assets/Scrips/Unspecified/MainDoorHandler.cs
+++ Assets/Scrips/Unspecified/MainDoorHandler.cs
@@ -8,7 +8,7 @@ public class DoorOpen : MonoBehaviour {
 	private Vector3 doorLeftPos;
 	private Vector3 doorRightPos;
 
-	private bool state = false; // true=open; false=closed
+	private byte playersNearby = 0;
 
 	void Start () {
 		doorLeftPos = doorLeft.transform.position;
@@ -16,18 +16,22 @@ public class DoorOpen : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		float left = Mathf.Lerp(doorLeft.transform.position.x, doorLeftPos.x + (state ? 1.5f/* open */ : 0/* closed */), Time.fixedDeltaTime * 2);
-		float right = Mathf.Lerp(doorRight.transform.position.x, doorRightPos.x + (state ? -1.5f/* open */ : 0/* closed */), Time.fixedDeltaTime * 2);
+		float doorLeftNewPos = Mathf.Lerp(doorLeft.transform.position.x, doorLeftPos.x + (hasPlayersNearby() ? 1.5f/* open */ : 0/* closed */), Time.fixedDeltaTime * 2);
+		float doorRightNewPos = Mathf.Lerp(doorRight.transform.position.x, doorRightPos.x + (hasPlayersNearby() ? -1.5f/* open */ : 0/* closed */), Time.fixedDeltaTime * 2);
 
-		doorLeft.transform.position = new Vector3(left, doorLeft.transform.position.y, doorLeft.transform.position.z);
-		doorRight.transform.position = new Vector3(right, doorRight.transform.position.y, doorRight.transform.position.z);
+		doorLeft.transform.position = new Vector3(doorLeftNewPos, doorLeft.transform.position.y, doorLeft.transform.position.z);
+		doorRight.transform.position = new Vector3(doorRightNewPos, doorRight.transform.position.y, doorRight.transform.position.z);
 	}
 
 	void OnTriggerEnter(Collider hit) {
-		state = true;
+		playersNearby++;
 	}
 
 	void OnTriggerExit(Collider hit) {
-		state = false;
+		playersNearby--;
+	}
+
+	private bool hasPlayersNearby() {
+		return playersNearby > 0;
 	}
 }
