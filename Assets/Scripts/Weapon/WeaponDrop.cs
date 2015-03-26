dc42bbe926830b3f2a330272d674f3a3c9775b72
Sandro Amaral <sandro123iv@gmail.com>
2015-03-26 16:03:27 +0000

    Implemented drop weapon. Still need to implement a way to grab it...

---
+++ Assets/Scripts/Weapon/WeaponDrop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponDrop : MonoBehaviour {
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.Q)) {
+			drop();
+		}
+	}
+
+	void drop() {
+		gameObject.AddComponent<Rigidbody>();
+
+		gameObject.transform.SetParent(null);
+	}
+}
